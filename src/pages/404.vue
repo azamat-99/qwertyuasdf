e79f48cc10e542dcdb274d6cd295c788ba400d29
Azamat <azamatrozumow@gmail.com>
2024-08-23 03:31:21 +0500

    404_page_added

---
+++ src/pages/404.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-800"
+  >
+    <el-result icon="error" title="404" sub-title="Oops! Page not found.">
+      <template #extra>
+        <el-button
+          type="primary"
+          size="large"
+          @click="goHome()"
+          class="transition-all duration-300 transform hover:scale-105"
+        >
+          Go to Home
+        </el-button>
+      </template>
+    </el-result>
+    <div class="mt-10 text-center text-gray-600">
+      <p class="text-lg">Sorry, the page you are looking for doesn't exist.</p>
+      <p class="text-sm mt-2">
+        You might have the wrong address, or the page may have moved.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ElResult, ElButton } from "element-plus";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const goHome = () => {
+  router.push({ name: "home" });
+};
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
+import Trans from "@/i18n/translation";
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../pages/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,32 +11,45 @@ const router = createRouter({
     };
   },
   routes: [
+    { path: "/", redirect: "/home" },
     {
-      path: "/",
-      name: "home",
-      component: HomeView,
+      path: "/:locale?",
+      component: () => import("../layout/default.vue"),
+      name: "layout",
+      beforeEnter: [Trans.routeMiddleware],
+      children: [
+        {
+          path: "home",
+          name: "home",
+          component: () => import("../pages/index.vue"),
+        },
+        {
+          path: "episode",
+          name: "episode",
+          component: () => import("../pages/episode/index.vue"),
+        },
+        {
+          path: "location",
+          name: "location",
+          component: () => import("../pages/location/index.vue"),
+        },
+        {
+          path: "episode/:id",
+          component: () => import("../pages/episode/[id].vue"),
+        },
+        {
+          path: "location/:id",
+          component: () => import("../pages/location/[id].vue"),
+        },
+        {
+          path: "character/:id",
+          component: () => import("../pages/character/[id].vue"),
+        },
+      ],
     },
     {
-      path: "/episode",
-      name: "episode",
-      component: () => import("../pages/episode/index.vue"),
-    },
-    {
-      path: "/location",
-      name: "location",
-      component: () => import("../pages/location/index.vue"),
-    },
-    {
-      path: "/episode/:id",
-      component: () => import("../pages/episode/[id].vue"),
-    },
-    {
-      path: "/location/:id",
-      component: () => import("../pages/location/[id].vue"),
-    },
-    {
-      path: "/character/:id",
-      component: () => import("../pages/character/[id].vue"),
+      path: "/:catchAll(.*)",
+      component: () => import("../pages/404.vue"),
     },
   ],
 });
